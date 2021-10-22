dd88c223d120adb704da718ef3565ead182ab188
Helton Rios <helton.placido@gmail.com>
2021-10-22 01:22:51 -0300

    aula 04

--- script.js
+++ script.js
@@ -11,9 +11,24 @@ function changeSocialMediaLinks() {
     const social = li.getAttribute('class')
 
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-
-    alert(li.children[0].href)
   }
 }
 
 changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
